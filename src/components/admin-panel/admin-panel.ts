c8e9d8260703e7bedb13a40785563d883fdf6625
lancelynyrd <pinedaclp@gmail.com>
2017-05-23 21:47:14 +0800

    updating user chat and admin chat

--- src/components/admin-panel/admin-panel.ts
+++ src/components/admin-panel/admin-panel.ts
@@ -1,180 +1,211 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
-import { App } from './../../providers/app';
-import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
-import { debounceTime } from 'rxjs/operator/debounceTime';
-import { } from 'jquery';
+import {Component, OnInit} from '@angular/core';
+import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
+import {App} from './../../providers/app';
+import {Observable} from 'rxjs/Observable';
+import {Subject} from 'rxjs/Subject';
+import {debounceTime} from 'rxjs/operator/debounceTime';
+import {} from 'jquery';
 @Component({
-    moduleId: module.id,
-    selector: 'admin-panel-component',
-    templateUrl: 'admin-panel.html',
-    styleUrls: ['./admin-panel.scss']
+  moduleId: module.id,
+  selector: 'admin-panel-component',
+  templateUrl: 'admin-panel.html',
+  styleUrls: ['./admin-panel.scss']
 })
 
 export class AdminPanelComponent implements OnInit {
-    uid: string = null;
-    all_message: FirebaseListObservable<any[]>;
-    user_message: FirebaseListObservable<any[]>;
-    last_message: FirebaseListObservable<any[]>;
-    form = {
-        message: ''
-    };
+  uid: string = null;
+  all_message: FirebaseListObservable<any[]>;
+  user_message: FirebaseListObservable<any[]>;
+  last_message: FirebaseListObservable<any[]>;
+  form = {
+    message: ''
+  };
 
-    username = '';      // username to chat.
-    secondCountDownForChatClose: number = 100;
-    observableForChatClose;
-    scrollMessage: Subject<any> = new Subject();
-    someoneTalking: boolean = false;
+  username = '';      // username to chat.
+  secondCountDownForChatClose: number = 100;
+  observableForChatClose;
+  scrollMessage: Subject<any> = new Subject();
+  someoneTalking: boolean = false;
 
-    config: boolean = false; // toogle admin config box.
-    constructor(
-        public db: AngularFireDatabase,
-        public app: App
-    ) {
+  config: boolean = false; // toogle admin config box.
+  chatDisplay: string = 'line';
 
-        this.uid = this.app.getClientId();
-        console.log("Chat User id: ", this.uid);
-        this.all_message = db.list('/messages/all/');
-        this.all_message.subscribe(res => {
-            //console.log(res);
-            // let obj = res.val();
+  user_last_list = [];
 
-            let node = res.pop();
-            let user = node['user'];
+  constructor(public db: AngularFireDatabase,
+              public app: App) {
 
-            if (this.username && this.username != user) {
+    this.uid = this.app.getClientId();
+    console.log("Chat User id: ", this.uid);
+    this.all_message = db.list('/messages/all/');
+    this.all_message.subscribe(res => {
+      //console.log(res);
+      // let obj = res.val();
 
-                //     console.log(`${this.username} : ${user}`);
-                this.someoneTalking = true;
-            }
-            this.scrollMessage.next();
+      let node = res.pop();
+      let user = node['user'];
+
+      if (this.username && this.username != user) {
+
+        //     console.log(`${this.username} : ${user}`);
+        this.someoneTalking = true;
+      }
+      this.scrollMessage.next();
+    });
+
+    // this.all_message.$ref.on('value', snapshot => {
+    //     let obj = snapshot.val();
+
+    //     let key = Object.keys(obj).pop();
+    //     let user = obj[key]['user'];
+
+    //     if (this.username && this.username != user) {
+
+    //         console.log(`${this.username} : ${user}`);
+    //         this.someoneTalking = true;
+    //     }
+    //     this.scrollMessage.next();
+    // });
+
+    this.scrollMessage
+      .debounceTime(100)
+      .subscribe(res => {
+        this.scrollMessageBox();
+      });
+
+
+  }
+
+  ngOnInit() {
+    this.last_message = this.db.list('/messages/last/', {
+      query: {
+        limitToLast: 5,
+        orderByChild: 'time'
+      }
+    });
+    this.last_message.subscribe(res => {
+      //console.log(res);
+      this.user_last_list = [];
+      for (let user of res) {
+        //console.log("user: ", user.$key);
+        this.db.list('/messages/users/' + user.$key, {
+          query: {
+            limitToLast: 1
+          }
+        }).subscribe( res => {
+          let lm = res[0];
+          //console.log('lm', lm);
+          if( lm ) lm['count'] = user.count ? user.count : '';
+          if ( lm ) this.user_last_list.push( lm );
+          // if ( res && res[0] && res[0].message) {
+          //   console.log("User chat: ", res[0].message, " Talk count: ", user.count);
+          // }
         });
+      }
+      console.log( "this.userlastlist:: ", this.user_last_list);
+      console.log("lastmessage::", this.last_message );
+    });
 
-        // this.all_message.$ref.on('value', snapshot => {
-        //     let obj = snapshot.val();
+    this.setChatNoOfLines();
+    this.setChatDisplay();
+  }
 
-        //     let key = Object.keys(obj).pop();
-        //     let user = obj[key]['user'];
+  onSubmitMessage() {
+    console.log("onSubmitMessage()");
 
-        //     if (this.username && this.username != user) {
+    this.user_message.push({user: 'admin', message: this.form.message});
 
-        //         console.log(`${this.username} : ${user}`);
-        //         this.someoneTalking = true;
-        //     }
-        //     this.scrollMessage.next();
-        // });
+    this.form.message = '';
+    this.countDownForChatClose();
+  }
 
-        this.scrollMessage
-            .debounceTime(100)
-            .subscribe(res => {
-                this.scrollMessageBox();
-            });
+  onClickUser(user) {
+    this.username = user;
+    this.user_message = this.db.list('/messages/users/' + user);
+    this.user_message.$ref.on('value', snapshot => {
+      // console.log(snapshot);
+      this.countDownForChatClose();
+      this.scrollMessage.next();
+    });
 
-        this.last_message = db.list('/messages/last/', {
-            query: {
-                limitToLast: 2,
-                orderByChild: 'time'
-            }
+    this.countDownForChatClose();
+  }
+
+  countDownForChatClose() {
+    if (this.observableForChatClose) this.observableForChatClose.unsubscribe();
+    this.observableForChatClose = Observable.interval(1000)
+      .take(100)
+      .subscribe(res => {
+          // console.log("res: ", res);
+          this.secondCountDownForChatClose = 100 - res;
+          // this.app.renderPage();
+        },
+        e => {
+        },
+        () => {
+          console.log("count complete");
+          this.username = '';
         });
-        this.last_message.subscribe(res => {
-            console.log(res);
-            for (let user of res) {
-                console.log("user: ", user.$key);
-                db.list('/messages/users/' + user.$key, {
-                    query: {
-                        limitToLast: 1
-                    }
-                }).subscribe(res => {
-                    if (res && res[0] && res[0].message) {
-                        console.log("User chat: ", res[0].message, " Talk count: ", user.count);
-                    }
-                });
-            }
-        });
+  }
 
+  onClickChatClose() {
+    this.observableForChatClose.unsubscribe();
+    this.username = '';
+    this.someoneTalking = false;
+    this.scrollMessage.next();
+  }
+
+  onChangeNoOfLines(event) {
+    this.setChatNoOfLines(event.target.value);
+  }
+
+  setChatNoOfLines(line?) {
+    if (line) {
+      localStorage.setItem('chatNoOfLines', line);
     }
+    else line = this.getOptionChatNoOfLines;
+    let $messages = $('.messages');
+    $messages.css('height', line + 'em');
+    this.scrollMessage.next();
+  }
 
-    ngOnInit() {
-        this.setChatNoOfLines();
+  setChatDisplay(display?) {
+    //console.log('display::',display);
+    if (display) {
+      localStorage.setItem('chatDisplay', display);
     }
-    onSubmitMessage() {
-        console.log("onSubmitMessage()");
+    else this.chatDisplay = this.getChatDisplay;
+  }
 
-        this.user_message.push({ user: 'admin', message: this.form.message });
-
-        this.form.message = '';
-        this.countDownForChatClose();
+  scrollMessageBox() {
+    let $messages = $('.messages');
+    if($messages && $messages[0].scrollHeight ){
+      $messages.animate({scrollTop: $messages[0].scrollHeight}, 300);
     }
+  }
 
-    onClickUser(user) {
-        this.username = user;
-        this.user_message = this.db.list('/messages/users/' + user);
-        this.user_message.$ref.on('value', snapshot => {
-            // console.log(snapshot);
-            this.countDownForChatClose();
-            this.scrollMessage.next();
-        });
+  get getOptionChatNoOfLines() {
+    let line;
+    line = localStorage.getItem('chatNoOfLines');
+    if (!line) line = 15;
+    return line;
+  }
 
-        this.countDownForChatClose();
-    }
-    countDownForChatClose() {
-        if (this.observableForChatClose) this.observableForChatClose.unsubscribe();
-        this.observableForChatClose = Observable.interval(1000)
-            .take(100)
-            .subscribe(res => {
-                // console.log("res: ", res);
-                this.secondCountDownForChatClose = 100 - res;
-                // this.app.renderPage();
-            },
-            e => { },
-            () => {
-                console.log("count complete");
-                this.username = '';
-            });
-    }
+  get minimized() {
+    let min = localStorage.getItem('minimized') ? true : false;
 
-    onClickChatClose() {
-        this.observableForChatClose.unsubscribe();
-        this.username = '';
-        this.someoneTalking = false;
-        this.scrollMessage.next();
-    }
+    //console.log( 'min: ', min );
+    return min;
+  }
 
-    onChangeNoOfLines(event) {
-        this.setChatNoOfLines(event.target.value);
-    }
+  set minimized(b) {
+    localStorage.setItem('minimized', (b ? 'y' : ''));
+  }
 
-    setChatNoOfLines(line?) {
-        if (line) {
-            localStorage.setItem('chatNoOfLines', line);
-        }
-        else line = this.getOptionChatNoOfLines;
-        let $messages = $('.messages');
-        $messages.css('height', line + 'em');
-        this.scrollMessage.next();
-    }
 
-    scrollMessageBox() {
-        let $messages = $('.messages');
-        $messages.animate({ scrollTop: $messages[0].scrollHeight }, 300);
-    }
+  get getChatDisplay() {
+    return localStorage.getItem('chatDisplay');
+  }
 
-    get getOptionChatNoOfLines() {
-        let line;
-        line = localStorage.getItem('chatNoOfLines');
-        if (!line) line = 15;
-        return line;
-    }
-
-    get minimized() {
-        let min = localStorage.getItem('minimized') ? true : false;
-
-        //console.log( 'min: ', min );
-        return min;
-    }
-    set minimized(b) {
-        localStorage.setItem('minimized', (b ? 'y' : ''));
-    }
 
 }
