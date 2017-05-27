7616999d763b388b0cb3a4a9f170f1112b30fb7b
JaeHo Song <thruthesky@gmail.com>
2017-05-27 16:56:14 +0800

    update for depcheck

--- depcheck.js
+++ depcheck.js
@@ -1,7 +1,52 @@
 var fs = require('fs');
+var dir = require('node-dir');
 
-var dirs = fs.readdirSync( 'src' );
 
-for( var f of dirs ) {
-    console.log(f);
-}
+dir.files('src', function (err, files) {
+    if (err) throw err;
+
+    var count = 0;
+
+    var re = /\.ts|scss|html$/;
+
+    var usedImages = [];
+
+    for (var f of files) {
+
+        if (!re.test(f)) continue;
+        count++;
+
+
+        var content = fs.readFileSync(f);
+        var text = content.toString(); // file content
+
+        var reImage = /([0-9a-zA-Z_\-]+\.(jpg|png|gif))/g;
+        var images = [];
+        while ((arr = reImage.exec(text)) !== null) {
+            var img = arr[0];
+            images.push( img );
+            usedImages.push( img );
+        }
+
+        if (images.length) {
+            console.log(`Found in : ${f}`);
+            console.log(images);
+            console.log("\n");
+        }
+
+    }
+
+
+    var A = files.filter( v => /\.(jpg|gif|png|psd)/.test( v ) );
+    var B = usedImages;
+    var C = A.filter( a => B.findIndex( b => a.indexOf( b ) != -1 ) == -1 );
+
+
+    console.log("Unused Images: ");
+    for (let f of C) {
+        console.log(f);
+    }
+
+});
+
+//
