1677b808427defbbc0e9a1fdb50f4b997f8177c4
JaeHo Song <thruthesky@gmail.com>
2017-05-29 21:41:09 +0800

    update for text

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,4 +1,8 @@
 export let name = "개발자 영어";
+export let name_l = name + "를"; // 을/를 중 하나를 선택해서, "을/를"을 끝에 붙인다. 예) 굿특을
+export let name_o = name + "는"; // 은/는 중 하나를 선택해서 끝에 붙인다. 예) 굿톡은
+
+
 export let footer = {
     text: "ⓒ 2017 개발자를 위한 화상영어 - 프로가 되기 위한 첫 걸음"
 };
--- src/components/footer/footer.ts
+++ src/components/footer/footer.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { App } from '../../providers/app';
-import * as config from './../../app/config';
 @Component({
     selector: 'footer-component',
     templateUrl: 'footer.html',
     styleUrls: ['footer.scss']
 })
 export class FooterComponent {
-    config = config;
     constructor( public app: App) {}
     onClickPanelMenu( name ) {
         this.app.scrollTo( name );
