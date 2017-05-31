ee77e205682eb63c447e3044bc7bed69ee6c9d3c
Manuel <manueljun17@gmail.com>
2017-05-31 14:31:35 +0800

    Remove Gender. No need for student.

--- src/components/modals/register/register.ts
+++ src/components/modals/register/register.ts
@@ -49,7 +49,6 @@ export class RegisterComponent {
             nickname: [ '', [Validators.required] ],
             mobile: [ ],
             birthday: [ ],
-            gender: [ ],
             id: [ '', [Validators.required] ],
         });
 
@@ -154,7 +153,6 @@ export class RegisterComponent {
                 name:this.userData.name,
                 nickname:this.userData.nickname,
                 mobile:this.userData.mobile,
-                gender:this.userData.gender,
                 email:this.userData.email?this.userData.email:''
             } );
             let birthday = this.getConcatBirthdate();
