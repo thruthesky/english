e9e50278e0197e1cd92f28940730ac2cddf7d7ee
lancelynyrd <pinedaclp@gmail.com>
2017-06-20 16:16:52 +0800

    adding site configuration

--- src/components/chat/chat.ts
+++ src/components/chat/chat.ts
@@ -4,6 +4,16 @@ import { User, _USER_DATA_RESPONSE, _USER_RESPONSE } from 'angular-backend';
 import { App } from './../../providers/app';
 import { FirebaseChat } from './../../providers/firebase';
 import { Subject } from 'rxjs/Subject';
+
+export interface _FIREBASE_CHAT {
+  user: string;
+  name: string;
+  message: string;
+  time?: number;
+  count?: number;
+  newVisitor?: boolean;
+}
+
 @Component({
   moduleId: module.id,
   selector: 'chat-component',
@@ -29,18 +39,26 @@ export class ChatComponent implements OnInit {
   ) {
     this.uid = this.app.getClientId();
     if (user.logged) this.userId = user.info.id;
-    this.scrollMessage
-      .debounceTime(100)
-      .subscribe(res => {
-        this.scrollMessageBox();
-      });
-
+    this.all_message = this.fc.getAllMessageList();
+    this.last_message = this.fc.getLastMessage();
     this.user_message = this.fc.getUserMessage(this.uid, {
       limitToLast: 10,
       orderByKey: true
     });
 
     this.user_message.subscribe(res => {
+      console.log('user_message:: ', res.length);
+      if ( res && res.length == 0 ) {
+        console.log('newVisitor');
+        let msg: _FIREBASE_CHAT = {
+          user: this.uid,
+          name: this.userId,
+          message: this.form.message,
+          newVisitor: true
+        };
+        this.pushMessage( msg );
+      }
+
       if (this.firstList) {
         this.firstList = false;
       }
@@ -49,37 +67,45 @@ export class ChatComponent implements OnInit {
       }
       this.scrollMessage.next();
     });
-    this.all_message = this.fc.getAllMessageList();
-    this.last_message = this.fc.getLastMessage();
+
+    this.scrollMessage
+      .debounceTime(100)
+      .subscribe(res => {
+        this.scrollMessageBox();
+    });
+
   }
+
   ngOnInit() {
   }
+
   onSubmitMessage() {
     if (this.form.message.length == 0) return;
-    let msg = {
+    let msg: _FIREBASE_CHAT = {
       user: this.uid,
       name: this.userId,
       message: this.form.message
     };
-    this.user_message.push(msg);
-    this.all_message.push(msg);
-    let $node = this.last_message.$ref['child'](this.uid);
-    $node.once("value", snapshot => {
-      let node = snapshot.val();
-      let count = 1;
-      if (node && node['count']) {
-        count = node['count'] + 1;
-      }
-      $node.set({
-        time: Math.floor(Date.now() / 1000),
-        count: count,
-        user: msg.user,
-        name: msg.name,
-        message: msg.message
-      });
-
-    });
+    this.pushMessage( msg );
     this.form.message = '';
+  }
+
+
+  pushMessage( msg: _FIREBASE_CHAT ) {
+      this.user_message.push(msg);
+      this.all_message.push(msg);
+      let $node = this.last_message.$ref['child'](this.uid);
+      $node.once("value", snapshot => {
+        let node = snapshot.val();
+        let count = 1;
+        if (node && node['count']) {
+          count = node['count'] + 1;
+        }
+
+        msg.time = Math.floor(Date.now() / 1000);
+        msg.count = count;
+        $node.set( msg );
+      });
   }
   onClickMinimize() {
     this.min = true;
