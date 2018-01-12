051724f169f9ece55fa7746fccb02178babdf1a8
lancelynyrd <pinedaclp@gmail.com>
2018-01-12 22:24:47 +0800

    update on intro

--- src/components/intro/intro.ts
+++ src/components/intro/intro.ts
@@ -11,7 +11,7 @@ export class IntroComponent {
     constructor(public app: App) {
         // if (app.classInfo) {
         //     if (app.classInfo.no_of_past && parseInt(app.classInfo.no_of_past, 10) > 7) { // students who has more than 7 past classes.
-                
+
         //         console.log( parseInt(app.classInfo.no_of_reservation, 10) );
         //         if ( parseInt(app.classInfo.no_of_reservation, 10) <= 3 ) { // and students who has less than 3 reservations
 
@@ -22,7 +22,13 @@ export class IntroComponent {
         //     }
 
 
-                
+
+    }
+
+
+    get fewClassLeft(): boolean {
+        return this.app.share.class_info && this.app.share.class_info['no_of_past'] &&
+            parseInt(this.app.share.class_info['no_of_past'], 10) >= 7 && parseInt(this.app.share.class_info['no_of_reservation'], 10) <= 3;
     }
 
 }
