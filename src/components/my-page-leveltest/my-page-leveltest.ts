5814c789963606fef70919d5d07419057716cf5f
JaeHo Song <thruthesky@gmail.com>
2018-01-21 21:55:57 +0800

    update on mypage

--- src/components/my-page-leveltest/my-page-leveltest.ts
+++ src/components/my-page-leveltest/my-page-leveltest.ts
@@ -10,26 +10,24 @@ import {DomSanitizer} from "@angular/platform-browser";
 })
 export class MyPageLeveltestComponent {
 
+
     error = null;
     loading = true;
-    data: {
-        max_score?: number;
-        no_of_classes?: number;
-        no_of_failure?: number;
-        no_of_success?: number;
-        rates?: {
-            expression?: number;
-            grammar?: number;
-            pronounciation?: number;
-            speed?: number;
-            vocabulary?: number;
-        }
-    } = {};
+
+    rates: {
+        expression?: number;
+        grammar?: number;
+        pronounciation?: number;
+        speed?: number;
+        vocabulary?: number;
+    };
+
+
     limit = 50;
     no_of_points = 1;
-    no_of_max_points = 1;
+    no_of_max_points = 45;
     performance_percentage = 1;
-    average_level = '';
+    average_level = '1';
 
     expression_percentage = 1;
     grammar_percentage = 1;
@@ -46,16 +44,17 @@ export class MyPageLeveltestComponent {
 
     single = false;
 
-
-
     constructor(public app: App,
                 private lms: LMS,
                 public sanitizer: DomSanitizer) {
 
-
         this.lms.getFirstAndLastClasses(data => {
             this.loading = false;
-
+            // if ( ! data) {
+            //     this.error = "앗, 수업 예약 정보가 없습니다.";
+            //     // return alert("LMS Information is empty...");
+            // }
+            console.log(data);
             if (data['first_class'] === void 0) {
                 this.error = "<div class='title'><i class='fa fa-warning'></i> 앗, 아직 수업을 하지 않았습니다.</div>" +
                     "<div class='desc'>레벨테스트는 정상적인 수업을 1개 이상 진행해야지만 결과를 볼 수 있습니다.</div>";
@@ -68,18 +67,50 @@ export class MyPageLeveltestComponent {
             if (data['last_class']) {
                 this.last_class = data['last_class'];
                 this.pre(data['last_class']);
+                // console.log("last_class", this.last_class);
+
+
+                this.rates = {
+                    expression: parseInt(this.last_class['rate_expression'], 10),
+                    grammar: parseInt(this.last_class['rate_grammar'], 10),
+                    speed: parseInt(this.last_class['rate_speed'], 10),
+                    vocabulary: parseInt(this.last_class['rate_vocabulary'], 10),
+                    pronounciation: parseInt(this.last_class['rate_pronounciation'], 10)
+                };
+
+
+                this.no_of_points = this.rates['expression'] + this.rates['grammar'] + this.rates['speed'] +
+                    this.rates['vocabulary'] + this.rates['pronounciation'];
+                this.performance_percentage = Math.floor(this.no_of_points * 100 / this.no_of_max_points);
+                // console.log("rate", this.rates);
+
+                this.expression_percentage = Math.floor(this.rates['expression'] * 100 / 9);
+                this.grammar_percentage = Math.floor(this.rates['grammar'] * 100 / 9);
+                this.pronounciation_percentage = Math.floor(this.rates['pronounciation'] * 100 / 9);
+                this.speed_percentage = Math.floor(this.rates['speed'] * 100 / 9);
+                this.vocabulary_percentage = Math.floor(this.rates['vocabulary'] * 100 / 9);
+
+                const level = Math.floor(this.performance_percentage / 100 * 9);
+                if (level === 9) {
+                    this.average_level = '8-9';
+                } else if (level === 0) {
+                    this.average_level = '1';
+                } else {
+                    this.average_level = `${level}-${level + 1}`;
+                }
             }
 
             if (this.first_class['idx'] == this.last_class['idx']) {
                 this.single = true;
             }
 
+            
 
-        }, () => {
+        }, error => {
             this.loading = false;
             this.error = "앗, 레벨테스트 정보를 가져오는 데 실패하였습니다. 서버 에러가 발생하였습니다.";
+            // alert("Error on retrieving the LMS Information" + error);
         });
-
 
     }
 
@@ -97,6 +128,5 @@ export class MyPageLeveltestComponent {
         const b = <string>session.class_begin;
         session.class_begin = b.substr(0, 2) + '시 ' + b.substr(2, 2) + '분';
     }
-
 
 }
